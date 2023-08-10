bb412afa731908779984accba4365839475cbca7
minjaekim <minjae2246@gmail.com>
2023-08-10 18:24:26 +0900

    ADD: 블로그 백엔드 저장소 초기 세팅 추가

---
+++ src/main/java/com/example/blogbeckend/BlogBeckendApplication.java
@@ -0,0 +1,13 @@
+package com.example.blogbeckend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BlogBeckendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BlogBeckendApplication.class, args);
+    }
+
+}
